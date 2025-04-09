12a0e149ae9bcf90ef7f3221b14e869b57627da5
ProcUQO <proc38@uqo.ca>
2025-04-08 22:36:44 -0400

    Détection de victoire, (horizontale, verticale, diagonale).

--- src/Model/Plateau.java
+++ src/Model/Plateau.java
@@ -11,6 +11,7 @@ public class Plateau {
         colonnes = new Jeton[NB_Colonnes][NB_Lignes];
     }
 
+    // Retourne la ligne du jeton qui vient d'être placé
     public int ajouterJeton(int colonne, Jeton jeton){
         if(colonne >= NB_Colonnes){
             return MOUVEMENT_INVALIDE; // Parce qu'il faut retourner quelque chose
@@ -30,8 +31,69 @@ public class Plateau {
         return colonnes[colonne][ligne];
     }
 
+    // Fonction de détection, inspiré de ce forum : https://stackoverflow.com/questions/32770321/connect-4-check-for-a-win-algorithm
+    // Utilise la ligne et la colonne du jeton qui vient d'être droppé
     public boolean verifierVictoire(int colonne, int ligneDuDernierJeton) { // nom un peu long, mais derniereLigne ou dernierJeton porte un peu à confusion
+        // On va prendre la couleur, pour ne pas faire n'importe quoi
+        String couleur = colonnes[colonne][ligneDuDernierJeton].getCouleur();
+
+        // Vérification horizontale
+        if(verifierDirection(colonne, ligneDuDernierJeton, couleur, 1, 0)) { // changement de colonne, même ligne
+            return true;
+        }
+        // Vérification verticale
+        if(verifierDirection(colonne, ligneDuDernierJeton, couleur, 0, 1)) { // changement de ligne, même colonne
+            return true;
+        }
+
+        // Vérification diagonale
+        if(verifierDirection(colonne, ligneDuDernierJeton, couleur, 1, 1)) { // vers le haut, à gauche
+            return true;
+        }
+
+        // Vérification diagonale (inverse)
+        if(verifierDirection(colonne, ligneDuDernierJeton, couleur, 1, -1)) { // vers bas, à gauche
+            return true;
+        }
 
         return false;
+    }
+
+    // Va être appelée 4 fois, vérifie les jetons dans une direction et dans la direction opposée.
+    private boolean verifierDirection(int colonne, int ligneDuDernierJeton, String couleur, int colonneDelta, int ligneDelta) {
+        int compteur = 1; // Le compteur du connect 4, qui commence à 1 parce qu'on compte le jeton qu'on vient de drop
+
+        // On vérifie dans la première direction
+        for(int i = 1; i < 4; i++){
+            int newColonne = colonne + i * colonneDelta;
+            int nouvelleLigne = ligneDuDernierJeton + i * ligneDelta;
+
+            // Le jeu fonctionne sans cette vérification, mais ça apaise la console. En fait, on ne veut pas vérifier à l'extérieur de la grille, car ça ne sert à rien et n'existe pas.
+            if(newColonne >= 0 && newColonne < NB_Colonnes && nouvelleLigne >= 0 && nouvelleLigne < NB_Lignes){
+                // On vérfiei si c'est la même couleur de jeton ( ou s'il y a une couleur tout court)
+                if (colonnes[newColonne][nouvelleLigne] != null && colonnes[newColonne][nouvelleLigne].getCouleur().equals(couleur)){
+                    compteur++;
+                } else {
+                    break; // On arrête (quand ce n'est pas un jeton de la même couleur
+                }
+            }
+
+        }
+
+        // Même logique, dans la direction opposée.
+        for(int i = 1; i < 4; i++){
+            int newColonne = colonne - i * colonneDelta;
+            int nouvelleLigne = ligneDuDernierJeton - i * ligneDelta;
+
+            if(newColonne >= 0 && newColonne < NB_Colonnes && nouvelleLigne >= 0 && nouvelleLigne < NB_Lignes){
+                if (colonnes[newColonne][nouvelleLigne] != null && colonnes[newColonne][nouvelleLigne].getCouleur().equals(couleur)){
+                    compteur++;
+                } else {
+                    break;
+                }
+            }
+        }
+
+        return compteur >= 4;
     }
 }
