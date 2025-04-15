cad05dac03ac83e05bb7a3c3ad7a9f8e88b894c5
ProcUQO <proc38@uqo.ca>
2025-04-15 10:50:18 -0400

    Ajout de nouveaux tests (diagonale, égalité, verticale, défaite)

--- src/Model/PlateauTest.java
+++ src/Model/PlateauTest.java
@@ -2,7 +2,6 @@ package Model;
 import org.junit.Assert;
 import org.junit.Test;
 
-import static org.junit.Assert.assertTrue;
 
 public class PlateauTest {
 
@@ -10,7 +9,7 @@ public class PlateauTest {
     @Test
     public void testVictoireHorizontale() {
         Plateau plateau = new Plateau();
-        Jeton jeton = new Jeton("Rouge");
+        Jeton jeton = new Jeton("Rouge"); // Je n'utilise pas directement l'héritage, mais c'est exceptionel (ce n'est qu'un test unitaire)
 
         // J'ajoute 4 jetons en ligne pour faire un connect 4. Je triche un peu, ils sont tous rouge.
         plateau.ajouterJeton(0, jeton);
@@ -19,7 +18,57 @@ public class PlateauTest {
         plateau.ajouterJeton(3, jeton);
 
         boolean victoire = plateau.verifierVictoire(3, 0);
-        assertTrue(victoire); // Vérifie si c'est vrai, pour les booléens
+        Assert.assertTrue(victoire); // Vérifie si c'est vrai, pour les booléens
+    }
+
+    @Test
+    public void testVictoireVerticale() {
+        Plateau plateau = new Plateau();
+        Jeton jeton = new Jeton("Jaune");
+
+        plateau.ajouterJeton(0, jeton);
+        plateau.ajouterJeton(0, jeton);
+        plateau.ajouterJeton(0, jeton);
+        plateau.ajouterJeton(0, jeton);
+
+        boolean victoire = plateau.verifierVictoire(0, 3);
+        Assert.assertTrue(victoire);
+    }
+
+    @Test
+    public void testVictoireDiagonale() {
+        Plateau plateau = new Plateau();
+        Jeton jetonRouge = new JetonRouge();
+        Jeton jetonJaune = new JetonJaune();
+
+        plateau.ajouterJeton(0, jetonRouge);
+        plateau.ajouterJeton(1, jetonJaune);
+        plateau.ajouterJeton(1, jetonRouge);
+        plateau.ajouterJeton(2, jetonJaune);
+        plateau.ajouterJeton(2, jetonJaune);
+        plateau.ajouterJeton(2, jetonRouge);
+        plateau.ajouterJeton(3, jetonJaune);
+        plateau.ajouterJeton(3, jetonJaune);
+        plateau.ajouterJeton(3, jetonJaune);
+        plateau.ajouterJeton(3, jetonRouge);
+
+        boolean victoire = plateau.verifierVictoire(3, 3);
+        Assert.assertTrue(victoire);
+    }
+
+    @Test
+    public void testPasFausseVictoire() {
+        Plateau plateau = new Plateau();
+        Jeton jetonRouge = new JetonRouge();
+        Jeton jetonJaune = new JetonJaune();
+
+        plateau.ajouterJeton(0, jetonRouge);
+        plateau.ajouterJeton(1, jetonRouge);
+        plateau.ajouterJeton(2, jetonRouge);
+        plateau.ajouterJeton(3, jetonJaune);
+
+        boolean victoire = plateau.verifierVictoire(3, 0);
+        Assert.assertFalse(victoire); // On s'assure que le jeu ne nous invente pas des victoires.
     }
 
 }
